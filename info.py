2f4554cbc6c81f489d5ba8ea48dfa93336fbd801
Unlimitedmovie <157523402+Unlimitedmovie@users.noreply.github.com>
2024-07-22 11:34:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
-BOT_TOKEN    = environ.get("BOT_TOKEN", "")
-DATABASE_URI = environ.get("DATABASE_URI", "")
+API_ID       = int(environ.get("API_ID", "28277294"))
+API_HASH     = environ.get("API_HASH", "ec1420b010830ad701cf518ce0474580")
+BOT_TOKEN    = environ.get("BOT_TOKEN", "7007928588:AAGRbAhFcchrJYKHNMVt2sgZN0fq8QtjfCQ")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://armantkd309:<password>@cluster0.9c5gzsg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002161305837"))
 ADMIN        = int(environ.get("ADMIN", "7065803173"))
 CHANNEL      = environ.get("CHANNEL", "@MovieWalaX")
